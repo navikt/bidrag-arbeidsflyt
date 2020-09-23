80b329ed9fe6e218161e8cd7dec71f2ced53cf09
millad.dagdoni@nav.no <541216bf17e99fdd00adce499023629fe503a74c>
2020-09-23 10:19:36 +0200

    legger til KafkaMeldingProducer.java for at prosjektet skal kompileres.

---
+++ src/main/java/no/nav/bidrag/arbeidsflyt/kafka/KafkaMeldingProducer.java
@@ -0,0 +1,30 @@
+package no.nav.bidrag.arbeidsflyt.kafka;
+
+import no.nav.bidrag.hendelse.producer.dto.RegistrerJournalpostDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaMeldingProducer {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(KafkaMeldingProducer.class);
+  private final String kafkaTopic;
+  private final KafkaTemplate<String, RegistrerJournalpostDto> kafkaTemplate;
+
+  public KafkaMeldingProducer(@Value("${kafka.topic}") String kafkaTopic,
+                              @Autowired KafkaTemplate<String, RegistrerJournalpostDto> kafkaTemplate) {
+    this.kafkaTopic = kafkaTopic;
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  public void sendMelding(int journalpostid, String saksnummer) {
+    LOGGER.info(String.format("Publiserer melding paa topic med journalpostid %s og saksnummer %s.", journalpostid, saksnummer));
+    this.kafkaTemplate.send(this.kafkaTopic, new RegistrerJournalpostDto(String.valueOf(journalpostid), saksnummer));
+  }
+}
