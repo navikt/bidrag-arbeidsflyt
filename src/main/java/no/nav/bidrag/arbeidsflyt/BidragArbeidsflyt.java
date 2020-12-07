4514cb5c607c1b87656999286ce61218c9a2b2f0
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-12-07 12:59:41 +0100

    added context path and prometheus, renamed test class

--- src/main/java/no/nav/bidrag/arbeidsflyt/BidragArbeidsflyt.java
+++ src/main/java/no/nav/bidrag/arbeidsflyt/BidragArbeidsflyt.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BidragArbeidsflytApplication {
+public class BidragArbeidsflyt {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BidragArbeidsflytApplication.class, args);
+		SpringApplication.run(BidragArbeidsflyt.class, args);
 	}
 
 }
--- src/test/java/no/nav/bidrag/arbeidsflyt/BidragArbeidsflytTests.java
+++ src/test/java/no/nav/bidrag/arbeidsflyt/BidragArbeidsflytTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BidragArbeidsflytApplicationTests {
+class BidragArbeidsflytTests {
 
 	@Test
 	void contextLoads() {
