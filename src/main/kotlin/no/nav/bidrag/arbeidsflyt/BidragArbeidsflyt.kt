b81e9809eff8aed6d192a4a19d904850606a21a2
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-12-07 11:53:05 +0100

    rename spring-boot application kotlin class

--- src/main/kotlin/no/nav/bidrag/arbeidsflyt/BidragArbeidsflyt.kt
+++ src/main/kotlin/no/nav/bidrag/arbeidsflyt/BidragArbeidsflyt.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BidragArbeidsflytApplication
+class BidragArbeidsflyt
 
 fun main(args: Array<String>) {
-	runApplication<BidragArbeidsflytApplication>(*args)
+	runApplication<BidragArbeidsflyt>(*args)
 }
