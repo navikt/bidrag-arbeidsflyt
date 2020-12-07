02ef27a9b331a8c86b469f88bf7eeb64dcdd22c7
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-12-07 12:08:07 +0100

    microservice in java, not kotlin :(

---
+++ src/main/java/no/nav/bidrag/arbeidsflyt/BidragArbeidsflytApplication.java
@@ -0,0 +1,13 @@
+package no.nav.bidrag.arbeidsflyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidragArbeidsflytApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidragArbeidsflytApplication.class, args);
+	}
+
+}
