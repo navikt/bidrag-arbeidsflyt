8243f172e1a4894089eba9b60017487049cb372c
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-12-14 14:05:52 +0100

    logging correlationId in thread
    - added after advice clearing correlationId from thread

--- src/main/kotlin/no/nav/bidrag/arbeidsflyt/aop/HendelseCorrelationAspect.kt
+++ src/main/kotlin/no/nav/bidrag/arbeidsflyt/aop/HendelseCorrelationAspect.kt
@@ -3,8 +3,10 @@ package no.nav.bidrag.arbeidsflyt.aop
 import no.nav.bidrag.arbeidsflyt.hendelse.JournalpostHendelse
 import no.nav.bidrag.commons.CorrelationId
 import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.annotation.After
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
+import org.slf4j.MDC
 import org.springframework.stereotype.Component
 
 @Component
@@ -13,6 +15,11 @@ class HendelseCorrelationAspect {
 
     @Before(value = "execution(* no.nav.bidrag.arbeidsflyt.service.BehandleHendelseService.*(..)) and args(journalpostHendelse)")
     fun addCorrelationIdToThread(joinPoint: JoinPoint, journalpostHendelse: JournalpostHendelse) {
-        CorrelationId.existing(journalpostHendelse.sporing?.correlationId)
+        MDC.put("correlationId", CorrelationId.existing(journalpostHendelse.sporing?.correlationId).get())
+    }
+
+    @After(value = "execution(* no.nav.bidrag.arbeidsflyt.service.BehandleHendelseService.*(..))")
+    fun clearCorrelationId(joinPoint: JoinPoint) {
+        MDC.clear()
     }
 }
