13641ffaaf83c0647e0817a7bc1db9e32afb2529
Ugur Cenar <cenar93@gmail.com>
2022-03-14 14:34:38 +0100

    Database config

--- src/main/resources/db/migration/V1.0.0__init_oppgave_table.sql
+++ src/main/resources/db/migration/V1.0.0__init_oppgave_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS oppgave (
     oppgave_id numeric NOT NULL,
-    journalpost_id varchar(20) NOT NULL,
-    status varchar(10) NOT NULL,
-    tema varchar(10) NOT NULL,
-    oppgavetype varchar(10) NOT NULL,
+    journalpost_id varchar(30) NOT NULL,
+    status varchar(20) NOT NULL,
+    tema varchar(20) NOT NULL,
+    oppgavetype varchar(20) NOT NULL,
     opprettet_timestamp timestamp DEFAULT now() NOT NULL,
     CONSTRAINT oppgave_pkey PRIMARY KEY (oppgave_id)
 );
