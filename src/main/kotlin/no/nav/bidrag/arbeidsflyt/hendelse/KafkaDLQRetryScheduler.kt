7fb7771f4e6ea3cbfcd6062563b8727899d64a81
Ugur Cenar <cenar93@gmail.com>
2022-05-19 10:28:51 +0200

    Kafka DLQ scheduler to retry failed messages

--- src/main/kotlin/no/nav/bidrag/arbeidsflyt/hendelse/KafkaDLQRetryScheduler.kt
+++ src/main/kotlin/no/nav/bidrag/arbeidsflyt/hendelse/KafkaDLQRetryScheduler.kt
@@ -1,5 +1,7 @@
 package no.nav.bidrag.arbeidsflyt.hendelse
 
+import no.nav.bidrag.arbeidsflyt.model.KunneIkkeProsessereKafkaMelding
+import no.nav.bidrag.arbeidsflyt.persistence.entity.DLQKafka
 import no.nav.bidrag.arbeidsflyt.persistence.repository.DLQKafkaRepository
 import no.nav.bidrag.arbeidsflyt.service.BehandleHendelseService
 import no.nav.bidrag.arbeidsflyt.service.BehandleOppgaveHendelseService
@@ -36,23 +38,32 @@ class KafkaDLQRetryScheduler(
         LOGGER.info("KafkaDLQRetryScheduler fant ${messages.size} meldinger som skal prosesseres")
 
         messages.stream().forEach {
-            LOGGER.info("Behandler melding ${it.id} med topicName ${it.topicName} og nøkkel ${it.messageKey} ")
-            when (it.topicName) {
-                topicOppgaveEndret -> {
-                    val oppgaveEndretHendelse = jsonMapperService.mapOppgaveHendelse(it.payload)
-                    behandleOppgaveHendelseService.behandleEndretOppgave(oppgaveEndretHendelse)
-                }
-                topicJournalpost -> {
-                    val journalpostHendelse = jsonMapperService.mapJournalpostHendelse(it.payload)
-                    behandleHendelseService.behandleHendelse(journalpostHendelse)
-                }
-                else -> {
-                    LOGGER.warn("Behandler ikke melding ${it.id} fordi det finnes ingen støtte for å behandle meldinger med topicName ${it.topicName} ")
-                }
+            try {
+                LOGGER.info("Behandler melding ${it.id} med topicName ${it.topicName} og nøkkel ${it.messageKey} ")
+                processMessage(it)
+                dlqKafkaRepository.delete(it)
+            } catch (e: Exception){
+                LOGGER.error("Det skjedde feil ved prosessering av melding ${it.id}", e)
+                it.retry = false
+                dlqKafkaRepository.save(it)
             }
-            it.retry = false
-            dlqKafkaRepository.save(it)
         }
 
+    }
+
+    fun processMessage(message: DLQKafka) {
+        when (message.topicName) {
+            topicOppgaveEndret -> {
+                val oppgaveEndretHendelse = jsonMapperService.mapOppgaveHendelse(message.payload)
+                behandleOppgaveHendelseService.behandleEndretOppgave(oppgaveEndretHendelse)
+            }
+            topicJournalpost -> {
+                val journalpostHendelse = jsonMapperService.mapJournalpostHendelse(message.payload)
+                behandleHendelseService.behandleHendelse(journalpostHendelse)
+            }
+            else -> {
+                throw KunneIkkeProsessereKafkaMelding("Behandler ikke melding ${message.id} fordi det finnes ingen støtte for å behandle meldinger med topicName ${message.topicName}")
+            }
+        }
     }
 }
--- src/main/kotlin/no/nav/bidrag/arbeidsflyt/model/exception.kt
+++ src/main/kotlin/no/nav/bidrag/arbeidsflyt/model/exception.kt
@@ -2,3 +2,5 @@ package no.nav.bidrag.arbeidsflyt.model
 
 class OpprettOppgaveFeiletFunksjoneltException(message: String, throwable: Throwable): RuntimeException(message, throwable)
 class EndreOppgaveFeiletFunksjoneltException(message: String, throwable: Throwable): RuntimeException(message, throwable)
+
+class KunneIkkeProsessereKafkaMelding(message: String): RuntimeException(message)
