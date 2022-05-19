cf08e3a8505b1f7f8943e50eb7badb4ab2e7cbcf
Ugur Cenar <cenar93@gmail.com>
2022-05-19 11:13:32 +0200

    Give access to shedlock table

--- src/main/resources/db/migration/V1.3.0__scheduler_lock_table.sql
+++ src/main/resources/db/migration/V1.3.0__scheduler_lock_table.sql
@@ -1,7 +1,9 @@
-CREATE TABLE shedlock (
+CREATE TABLE IF NOT EXISTS shedlock (
     name VARCHAR(64) NOT NULL,
     lock_until TIMESTAMP NOT NULL,
     locked_at TIMESTAMP NOT NULL,
     locked_by VARCHAR(255) NOT NULL,
     PRIMARY KEY (name)
 );
+
+GRANT ALL PRIVILEGES ON TABLE public.shedlock TO cloudsqliamuser;
