ab36c6cfc5efb5256a943772d2a05c4d76f31957
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2023-04-27 16:09:08 +0200

    Spring boot 3 (#184)

--- src/main/kotlin/no/nav/bidrag/arbeidsflyt/persistence/entity/Oppgave.kt
+++ src/main/kotlin/no/nav/bidrag/arbeidsflyt/persistence/entity/Oppgave.kt
@@ -1,9 +1,9 @@
 package no.nav.bidrag.arbeidsflyt.persistence.entity
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
 import no.nav.bidrag.arbeidsflyt.dto.OppgaveHendelse
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
 
 @Entity
 data class Oppgave(
