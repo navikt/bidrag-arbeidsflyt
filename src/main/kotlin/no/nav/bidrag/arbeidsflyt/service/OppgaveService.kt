5a700a1145be70f5e3408eac9545a84368f61808
Ugur Alpay Cenar <cenar93@gmail.com>
2026-05-22 09:48:34 +0200

    Oppgrader til SB4

--- src/main/kotlin/no/nav/bidrag/arbeidsflyt/service/OppgaveService.kt
+++ src/main/kotlin/no/nav/bidrag/arbeidsflyt/service/OppgaveService.kt
@@ -84,7 +84,7 @@ class OppgaveService(
                     ).oppgaver
             } else {
                 emptyList()
-            }.filter { søknadId == null || it.søknadsid != null && it.søknadsid!!.toLong() == søknadId }
+            }.filter { søknadId == null || (it.søknadsid != null && it.søknadsid!!.toLong() == søknadId) }
         val oppgaverForSøknad =
             if (søknadId != null) {
                 oppgaveConsumer
